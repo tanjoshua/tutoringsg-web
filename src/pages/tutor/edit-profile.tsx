02333d35aff11a66f8eadda03b62e170b7f17947
tanjoshua <jtanjoshua@gmail.com>
2023-02-20 15:17:30 -0500

    loading for create profile button

--- src/pages/tutor/edit-profile.tsx
+++ src/pages/tutor/edit-profile.tsx
@@ -385,7 +385,7 @@ const EditProfile: NextPageWithLayout = () => {
               type="submit"
               className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
-              Make Edits
+              Edit profile
             </button>
           </div>
         )}
