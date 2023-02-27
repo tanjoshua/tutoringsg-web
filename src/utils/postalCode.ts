0e220e5979f3af86c9161e9e86969cb603e45f76
tanjoshua <jtanjoshua@gmail.com>
2023-02-27 12:16:30 -0500

    postal code to region

---
+++ src/utils/postalCode.ts
@@ -0,0 +1,17 @@
+import { Region } from "./enums";
+
+export const postalCodeToRegion = (postalCode: string) => {
+  // might wanna replace with more districts in the future
+  const first2digits = parseInt(postalCode.slice(0, 2));
+  if (first2digits < 34) {
+    return Region.Central;
+  } else if (first2digits < 53) {
+    return Region.East;
+  } else if (first2digits < 58) {
+    return Region.NorthEast;
+  } else if (first2digits < 72) {
+    return Region.West;
+  } else {
+    return Region.North;
+  }
+};
