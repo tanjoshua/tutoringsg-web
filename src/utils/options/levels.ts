291fdc4e37c9296a17b34156c0635d6ce4086c04
tanjoshua <jtanjoshua@gmail.com>
2023-03-14 17:35:20 -0400

    create subjects by level

---
+++ src/utils/options/levels.ts
@@ -0,0 +1,73 @@
+export const PrimaryLevels = [
+  "Primary 1",
+  "Primary 2",
+  "Primary 3",
+  "Primary 4",
+  "Primary 5",
+  "Primary 6",
+];
+
+export const LowerSecondaryLevels = [
+  // Sec 1
+  "Secondary 1 (Express)",
+  "Secondary 1 (Normal Academic)",
+  "Secondary 1 (Normal Technical)",
+  "Secondary 1 (Integrated Program)",
+
+  // Sec 2
+  "Secondary 2 (Express)",
+  "Secondary 2 (Normal Academic)",
+  "Secondary 2 (Normal Technical)",
+  "Secondary 2 (Integrated Program)",
+];
+
+export const UpperSecondaryLevels = [
+  // Sec 3
+  "Secondary 3 (Express)",
+  "Secondary 3 (Normal Academic)",
+  "Secondary 3 (Normal Technical)",
+  "Secondary 3 (Integrated Program)",
+
+  // Sec 4
+  "Secondary 4 (Express)",
+  "Secondary 4 (Normal Academic)",
+  "Secondary 4 (Normal Technical)",
+  "Secondary 4 (Integrated Program)",
+];
+
+export const JCLevels = [
+  "Junior College 1 (JC1)",
+  "Junior College 2 (JC2)",
+  "MI Year 3",
+];
+
+export const levelOptions = [
+  {
+    label: "Primary",
+    options: PrimaryLevels.map((level) => ({
+      label: level,
+      value: level,
+    })),
+  },
+  {
+    label: "Lower Secondary",
+    options: LowerSecondaryLevels.map((level) => ({
+      label: level,
+      value: level,
+    })),
+  },
+  {
+    label: "Upper Secondary",
+    options: UpperSecondaryLevels.map((level) => ({
+      label: level,
+      value: level,
+    })),
+  },
+  {
+    label: "JC",
+    options: JCLevels.map((level) => ({
+      label: level,
+      value: level,
+    })),
+  },
+];
---
+++ src/utils/options/subjects.ts
@@ -0,0 +1,105 @@
+import {
+  JCLevels,
+  LowerSecondaryLevels,
+  PrimaryLevels,
+  UpperSecondaryLevels,
+} from "./levels";
+
+const PrimarySubjects = [
+  "English",
+  "Math",
+  "Science",
+  "Chinese",
+  "Malay",
+  "Tamil",
+  "Hindi",
+];
+
+const LowerSecondarySubjects = [
+  "English",
+  "Math",
+  "Science",
+  "History",
+  "Geography",
+  "Social Studies",
+  "Literature",
+  "Chinese",
+  "Tamil",
+  "Malay",
+  "Hindi",
+];
+
+const UpperSecondarySubjects = [
+  "English",
+  "E Math",
+  "A Math",
+  "Science",
+  "Physics",
+  "Chemistry",
+  "Biology",
+  "History",
+  "Geography",
+  "Social Studies",
+  "Literature",
+  "Chinese",
+  "Tamil",
+  "Malay",
+  "Hindi",
+  "Principle of Accounts",
+];
+
+const JCSubjects = [
+  "General Paper",
+  "Knowledge and Inquiry",
+  "Math",
+  "Physics",
+  "Chemistry",
+  "Biology",
+  "Economics",
+  "History",
+  "Geography",
+  "Literature",
+  "Computing",
+  "Art",
+  "Chinese",
+  "Tamil",
+  "Malay",
+  "Hindi",
+  "Principle of Accounts",
+];
+
+export const PrimarySubjectOptions = PrimarySubjects.map((subject) => ({
+  label: subject,
+  value: subject,
+}));
+export const LowerSecondarySubjectOptions = LowerSecondarySubjects.map(
+  (subject) => ({
+    label: subject,
+    value: subject,
+  })
+);
+export const UpperSecondarySubjectOptions = UpperSecondarySubjects.map(
+  (subject) => ({
+    label: subject,
+    value: subject,
+  })
+);
+export const JCSubjectOptions = JCSubjects.map((subject) => ({
+  label: subject,
+  value: subject,
+}));
+
+// helper functions
+export const getSubjectOptions = (level: string) => {
+  if (level in PrimaryLevels) {
+    return PrimarySubjectOptions;
+  } else if (level in LowerSecondaryLevels) {
+    return LowerSecondarySubjectOptions;
+  } else if (level in UpperSecondaryLevels) {
+    return UpperSecondarySubjectOptions;
+  } else if (level in JCLevels) {
+    return JCSubjectOptions;
+  } else {
+    return [];
+  }
+};
