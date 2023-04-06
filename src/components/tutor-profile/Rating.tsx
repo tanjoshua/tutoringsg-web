8ddf1da4bf4cc657f82e41a933e5275d5166d3d7
tanjoshua <jtanjoshua@gmail.com>
2023-04-06 18:47:05 -0400

    rating - if i ever need it

---
+++ src/components/tutor-profile/Rating.tsx
@@ -0,0 +1,73 @@
+export default function Rating({
+  rating,
+}: {
+  rating: number;
+  rateCount: number;
+}) {
+  let roundedRating = Math.round(rating * 2) / 2;
+  const stars: JSX.Element[] = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (roundedRating >= 1) {
+      stars.push(
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5 text-yellow-400"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Full star</title>
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+      );
+    } else if (roundedRating === 0.5) {
+      stars.push(
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient id="halfgrad">
+              <stop offset="50%" stop-color="rgb(250 204 21)" />
+              <stop offset="50%" stop-color="rgb(107 114 128)" />
+            </linearGradient>
+          </defs>
+          <title>Half star</title>
+          <path
+            fill="url(#halfgrad)"
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          />
+        </svg>
+      );
+    } else {
+      // empty star
+      stars.push(
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5 text-gray-300 dark:text-gray-500"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Empty star</title>
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+      );
+    }
+
+    roundedRating -= 1;
+  }
+
+  return (
+    <div className="flex items-center">
+      {stars.map((star) => star)}
+      <p className="ml-2 text-sm font-medium text-gray-500">
+        {rating.toFixed(2)}
+      </p>
+    </div>
+  );
+}
